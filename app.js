9bfdca972b098aacf14b4180e4e8b0915f4741c7
al-amin <alamin931761@gmail.com>
2022-10-10 20:29:45 +0600

    shadow

--- app.js
+++ app.js
@@ -12,25 +12,25 @@ document.getElementById('add-button').addEventListener('click', function () {
         <th scope="row">${number}</th>
         <td>${inputField}</td>
         <td>
-        <button class='btn btn-danger delete-button'>Delete</button>
         <button class='btn btn-success done-button'>Done</button>
+        <button class='btn btn-danger delete-button'>Delete</button>
         </td>`;
         tableContent.appendChild(tableRow);
         document.getElementById('input-field').value = '';
-
-        // delete button
-        const deleteButton = document.getElementsByClassName('delete-button');
-        for (const button of deleteButton) {
-            button.addEventListener('click', function (event) {
-                event.target.parentNode.parentNode.style.display = 'none';
-            });
-        };
 
         // done button
         const doneButton = document.getElementsByClassName('done-button');
         for (const button of doneButton) {
             button.addEventListener('click', function (event) {
                 event.target.parentNode.parentNode.style.textDecoration = 'line-through';
+            });
+        };
+
+        // delete button
+        const deleteButton = document.getElementsByClassName('delete-button');
+        for (const button of deleteButton) {
+            button.addEventListener('click', function (event) {
+                event.target.parentNode.parentNode.style.display = 'none';
             });
         };
 
